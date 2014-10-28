66a44a0aaf07e2b71024843de44a418cef7abec1
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-10-28 19:49:17 +0100

    Part 2 - The dot

--- js/main.js
+++ js/main.js
@@ -1,3 +1,7 @@
 function FirstController($scope){
-  $scope.data = {message: 'Hello'}; // Try to delete 'hello' and add 'panel' or 'label' to see the effects
+  // $scope.data = {message: 'Hello'}; // Try to delete 'hello' and add 'panel' or 'label' to see the effects
+}
+
+function SecondController($scope){
+  // $scope.data = {message: 'Hello'}; // Try to delete 'hello' and add 'panel' or 'label' to see the effects
 }
