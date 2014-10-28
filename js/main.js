a81f2c1060d7a6b91b4e7fe937a5687ee79144f5
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-10-28 20:12:24 +0100

    Part 2 - Sharing Data between controlers

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
-function FirstController($scope){
+var myApp = angular.module('myApp', []);
+myApp.factory('myData', function(){ // Inject this data into the two controllers created using the SERVICE 'factory'.
+  return {message: 'I am data from a service'}
+});
+
+function FirstController($scope, myData){
   // $scope.data = {message: 'Hello'}; // Try to delete 'hello' and add 'panel' or 'label' to see the effects
+  $scope.date = myData;
 }
 
-function SecondController($scope){
+function SecondController($scope, myData){
   // $scope.data = {message: 'Hello'}; // Try to delete 'hello' and add 'panel' or 'label' to see the effects
+  $scope.date = myData;
 }
