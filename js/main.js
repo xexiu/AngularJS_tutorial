b55649f2420cedde33afcb41df78c8e2734a9ba5
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-10-28 21:48:11 +0100

    Part 2 - Defining a method on the scope

--- js/main.js
+++ js/main.js
@@ -1,14 +1,18 @@
 var myApp = angular.module('myApp', []);
 myApp.factory('myData', function(){ // Inject this data into the two controllers created using the SERVICE 'factory'.
-  return {message: 'I am data from a service'}
+  return {message: 'I am data from a service'};
 });
 
 function FirstController($scope, myData){
   // $scope.data = {message: 'Hello'}; // Try to delete 'hello' and add 'panel' or 'label' to see the effects
-  $scope.date = myData;
+  $scope.data = myData;
 }
 
 function SecondController($scope, myData){
   // $scope.data = {message: 'Hello'}; // Try to delete 'hello' and add 'panel' or 'label' to see the effects
-  $scope.date = myData;
+  $scope.data = myData;
+
+  $scope.reversedMessage = function(message){
+    return message.split('').reverse().join('');
+  };
 }
