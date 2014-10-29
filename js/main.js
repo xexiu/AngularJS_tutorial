ed1813dac8344440d1ada2027da7ce9e86f5ad3c
Sergio Mironescu <sergiu.mironescu@gmail.com>
2014-10-29 09:27:27 +0100

    Part 3. Filters

--- js/main.js
+++ js/main.js
@@ -3,6 +3,12 @@ myApp.factory('myData', function(){ // Inject this data into the two controllers
   return {message: 'I am data from a service'};
 });
 
+myApp.filter('reverse', function(myData) {
+  return function(text){
+    return text.split('').reverse().join('') + ' => ' + myData.message;
+  };
+});
+
 function FirstController($scope, myData){
   // $scope.data = {message: 'Hello'}; // Try to delete 'hello' and add 'panel' or 'label' to see the effects
   $scope.data = myData;
@@ -12,7 +18,7 @@ function SecondController($scope, myData){
   // $scope.data = {message: 'Hello'}; // Try to delete 'hello' and add 'panel' or 'label' to see the effects
   $scope.data = myData;
 
-  $scope.reversedMessage = function(message){
+  /* $scope.reversedMessage = function(message){
     return message.split('').reverse().join('');
-  };
+  }; */
 }
